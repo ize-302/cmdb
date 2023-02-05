65a5536e85f54a94151adacb2c76e4e1aafcde3f
ize-send <adavize.hassan@send.ng>
2023-02-05 15:40:44 +0100

    cleanup

--- background/index.ts
+++ background/index.ts
@@ -20,20 +20,6 @@ chrome.commands.onCommand.addListener((command, tab) => {
   toggleExtension(tab.id);
 });
 
-// first time isntlalation
-chrome.runtime.onInstalled.addListener(function () {
-  alert(
-    "You just made the best decision of today, by installing GMass!\n\nWe will now redirect you to your Gmail account so you can get started sending email campaigns inside Gmail."
-  );
-
-  chrome.tabs.create({
-    url: "https://mail.google.com",
-    active: true,
-  });
-
-  return false;
-});
-
 // Receive messages
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
   if (message === "CMDB_FETCH_BOOKMARKS") {
